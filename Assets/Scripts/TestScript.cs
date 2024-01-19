ee8b0e680b6a0412ed23865471e964dc835ba3c9
MiMiMi <105704477+right-of-earphone@users.noreply.github.com>
2024-01-19 12:37:05 +0900

    test kano

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("Žë–ì—T‘¾‚Å‚·");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
